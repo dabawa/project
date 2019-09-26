fe026c1149b1c33ac6124e11c49d06efd9a4f229
Dabawa Andrew <andrewdabawa93@gmail.com>
2019-09-25 23:46:19 -0700

    workin on carousel 2 slide

--- pages/index.php
+++ pages/index.php
@@ -13,143 +13,94 @@
     
 <nav class="navbar navbar-expand-sm  navbar-dark fixed-top bg-dark p-1">
 
-<a class="navbar-brand px-5" href="#">MY LOGO</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navNavbar" >
-  <span class="navbar-toggler-icon"></span>
-    </button>
-            
-        
-        <div class="collapse navbar-collapse" id="navNavbar">
-        <ul class="navbar-nav" >
-            <li class="nav-item px-2">
-                <a href="#home" class="nav-link ">Home</a>
-            </li>
-            <li class="nav-item px-2">
-                <a href="#about" class="nav-link">About Us</a>
-            </li>
-            <li class="nav-item px-2">
-                <a href="#services" class="nav-link">Services</a>
-            </li>
-            <li class="nav-item px-2">
-                <a href="#contact" class="nav-link">Contact Us</a>
-            </li>
-        </ul>
-
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item dropdown mr-3">
-        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-          <i class="fa fa-user"></i> Welcome Andrew
-        </a>
-         <div class="dropdown-menu">
-           <a href="profile2.html" class="dropdown-item">
-           <i class="fa fa-user-circle"></i>Profile
-         </a>
-          <a href="settings.html" class="dropdown-item">
-              <i class="fa fa-gear"></i> Settings
-            </a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a href="coverpage2.html" class="nav-link">
-            <i class="fa fa-user-times"></i> Login
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="coverpage2.html" class="nav-link">
-            <i class="fa fa-user-times"></i> Sign up
-          </a>
-        </li>
-   
-    </ul>
-
-    </div>
-      </nav>
-    
-  <section id="showcase">
-    <div class="">
-    <div class="row">
-      <div class="col-sm-12">
-    
-      <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1" ></li>
-        <li data-target="#myCarousel" data-slide-to="2" ></li>
-        <li data-target="#myCarousel" data-slide-to="3" ></li>
-        <li data-target="#myCarousel" data-slide-to="4" ></li>
-        <li data-target="#myCarousel" data-slide-to="5" ></li>
-      </ol> 
-   <div class="carousel-inner">
-     <div class="carousel-item carousel-image-1 active">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-center mb-5"> 
-           <p class="lead">Let's solve your electricity problem</p> 
-         </div>
-       </div>
-     </div>
-
-
-       <div class="carousel-item carousel-image-2">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-center mb-5">
+  <a class="navbar-brand px-5" href="#">MY LOGO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navNavbar" >
+    <span class="navbar-toggler-icon"></span>
+      </button>
+              
           
-         </div>
-       </div>
-     </div>
+          <div class="collapse navbar-collapse" id="navNavbar">
+          <ul class="navbar-nav" >
+              <li class="nav-item px-2">
+                  <a href="#home" class="nav-link ">Home</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a href="#about" class="nav-link">About Us</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a href="#services" class="nav-link">Services</a>
+              </li>
+              <li class="nav-item px-2">
+                  <a href="#contact" class="nav-link">Contact Us</a>
+              </li>
+          </ul>
 
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown mr-3">
+          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-user"></i> Welcome Andrew
+          </a>
+          <div class="dropdown-menu">
+            <a href="profile2.html" class="dropdown-item">
+            <i class="fa fa-user-circle"></i>Profile
+          </a>
+            <a href="settings.html" class="dropdown-item">
+                <i class="fa fa-gear"></i> Settings
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a href="coverpage2.html" class="nav-link">
+              <i class="fa fa-user-times"></i> Login
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="coverpage2.html" class="nav-link">
+              <i class="fa fa-user-times"></i> Sign up
+            </a>
+          </li>
+    
+      </ul>
 
-       <div class="carousel-item carousel-image-3">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-left mb-5">
-           <p class="lead">Life is good when there's Electricity</p>
-         </div>
-       </div>
-     </div>
-       
+      </div>
+    </nav>
+    
+  
+    <section>
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-        <div class="carousel-item carousel-image-4">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-right mb-5">
-           
-         </div>
-       </div>
-     </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="../images/post_14.jpg" alt="Los Angeles">
+          </div>
 
+          <div class="item">
+            <img src="../images/post_15.jpg" alt="Chicago">
+          </div>
 
-      <div class="carousel-item carousel-image-5">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-right mb-5">
-        
-         </div>
-       </div>
-     </div>
+          <div class="item">
+            <img src="../images/post_16.jpg" alt="New York">
+          </div>
+        </div>
 
-
-       <div class="carousel-item carousel-image-6">
-       <div class="container">
-         <div class="carousel-caption d-none d-sm-block text-right mb-5">
-         
-         </div>
-       </div>
-     </div>
-
-
-     </div>
-
-     <a href="#myCarousel" data-slide="prev" class="carousel-control-prev">
-       <span class="carousel-control-prev-icon"></span>
-     </a>
-      <a href="#myCarousel" data-slide="next" class="carousel-control-next">
-       <span class="carousel-control-next-icon"></span>
-     </a> 
-
-
-    </div>
-
-   </div>
-   </div>
-  </div>
-</section>
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </section>
 
 
 
